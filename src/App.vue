a11c5c8905def0ba369e63490fffa26fa7ca8cb9
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-13 18:05:27 +0200

    Add component for main

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import SiteHeader from './components/SiteHeader.vue'
+import SiteMain from './components/SiteMain.vue'
 
 export default {
   components: {
     SiteHeader,
+    SiteMain,
   },
   data() {
     return {
@@ -16,40 +18,7 @@ export default {
 <template>
 
   <SiteHeader></SiteHeader>
-  
-  <main id="app_main">
-    <div class="main_top">
-      <div class="container">
-        <div class="py-5">Content goes here</div>
-      </div>
-    </div>
-    <div class="main_bottom">
-      <div class="container">
-        <div class="row justify-content-evenly">
-          <div class="col-2 d-flex align-items-center">
-            <img src="./assets/img/buy-comics-digital-comics.png" alt="" />
-            <div class="ms-1">Digital Comics</div>
-          </div>
-          <div class="col-2 d-flex align-items-center">
-            <img src="./assets/img/buy-comics-merchandise.png" alt="" />
-            <div class="ms-1">Merchandise</div>
-          </div>
-          <div class="col-2 d-flex align-items-center">
-            <img src="./assets/img/buy-comics-shop-locator.png" alt="" />
-            <div class="ms-1">Shop location</div>
-          </div>
-          <div class="col-2 d-flex align-items-center">
-            <img src="./assets/img/buy-comics-subscriptions.png" alt="" />
-            <div class="ms-1">Subscription</div>
-          </div>
-          <div class="col-2 d-flex align-items-center">
-            <img src="./assets/img/buy-dc-power-visa.svg" alt="" />
-            <div class="ms-1">DC Visa</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+  <SiteMain></SiteMain>
 
   <footer id="app_footer">
     <div class="footer_top p-3">
---
+++ src/components/SiteMain.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    name: "SiteMain"
+}
+</script>
+
+<template>
+    <main id="app_main">
+    <div class="main_top">
+      <div class="container">
+        <div class="py-5">Content goes here</div>
+      </div>
+    </div>
+    <div class="main_bottom">
+      <div class="container">
+        <div class="row justify-content-evenly">
+          <div class="col-2 d-flex align-items-center">
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+            <div class="ms-1">Digital Comics</div>
+          </div>
+          <div class="col-2 d-flex align-items-center">
+            <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+            <div class="ms-1">Merchandise</div>
+          </div>
+          <div class="col-2 d-flex align-items-center">
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+            <div class="ms-1">Shop location</div>
+          </div>
+          <div class="col-2 d-flex align-items-center">
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+            <div class="ms-1">Subscription</div>
+          </div>
+          <div class="col-2 d-flex align-items-center">
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+            <div class="ms-1">DC Visa</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
