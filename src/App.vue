f41295ae7407cfc0486f9587a3e7e14ddebd1140
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-13 15:55:35 +0200

    Add footer details and fix main

--- src/App.vue
+++ src/App.vue
@@ -30,38 +30,93 @@ export default {
   </header>
 
   <main id="app_main">
-    <div class="container_main">
-      <div class="main_content">
-        <h3>Content goes here</h3>
+    <div class="main_top">
+      <div class="container_main">
+        <div class="main_content">
+          <h3>Content goes here</h3>
+        </div>
       </div>
     </div>
-    <div class="blue_banner">
-      <div>
-        <img src="./assets/img/buy-comics-digital-comics.png" alt="">
-        <span>Digital Comics</span>
-      </div>
-      <div>
-        <img src="./assets/img/buy-comics-merchandise.png" alt="">
-        <span>Merchandise</span>
-      </div>
-      <div>
-        <img src="./assets/img/buy-comics-shop-locator.png" alt="">
-        <span>Shop location</span>
-      </div>
-      <div>
-        <img src="./assets/img/buy-comics-subscriptions.png" alt="">
-        <span>Subscription</span>
-      </div>
-      <div>
-        <img src="./assets/img/buy-dc-power-visa.svg" alt="">
-        <span>DC Visa</span>
+    <div class="main_bottom">
+      <div class="container_main">
+        <div class="blue_banner">
+          <div>
+            <img src="./assets/img/buy-comics-digital-comics.png" alt="" />
+            <span>Digital Comics</span>
+          </div>
+          <div>
+            <img src="./assets/img/buy-comics-merchandise.png" alt="" />
+            <span>Merchandise</span>
+          </div>
+          <div>
+            <img src="./assets/img/buy-comics-shop-locator.png" alt="" />
+            <span>Shop location</span>
+          </div>
+          <div>
+            <img src="./assets/img/buy-comics-subscriptions.png" alt="" />
+            <span>Subscription</span>
+          </div>
+          <div>
+            <img src="./assets/img/buy-dc-power-visa.svg" alt="" />
+            <span>DC Visa</span>
+          </div>
+        </div>
       </div>
     </div>
   </main>
 
-  
-
-  <footer id="app_footer"></footer>
+  <footer id="app_footer">
+    <div class="footer_top">
+      <div>
+        <div id="dc_comics">
+          <h5>DC COMICS</h5>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+        </div>
+        <div id="shop">
+          <h5>SHOP</h5>
+          <ul>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
+        </div>
+      </div>
+      <div id="dc">
+        <h5>DC</h5>
+        <ul>
+          <li>Terms Of Use</li>
+          <li>Privacy policy(new)</li>
+          <li>Ad choices</li>
+          <li>Advertising</li>
+          <li>jobs</li>
+          <li>Subscriptions</li>
+          <li>News</li>
+          <li>Subscriptions</li>
+          <li>News</li>
+          <li>Subscriptions</li>
+          <li>News</li>
+        </ul>
+      </div>
+      <div id="sites">
+        <h5>SITES</h5>
+        <ul>
+          <li>Terms Of Use</li>
+          <li>Privacy policy(new)</li>
+          <li>Ad choices</li>
+          <li>Advertising</li>
+          <li>jobs</li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer_bottom"></div>
+  </footer>
 </template>
 
 <style scoped></style>
