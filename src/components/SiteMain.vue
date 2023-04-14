23019caa61cedde01a34845cb8b063de58d63553
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-14 17:05:41 +0200

    Add button and "current series" to SiteMain

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -23,16 +23,35 @@ export default {
 
   <div class="main_top">
     <div class="container">
+      <div id="title">
+        <h3>CURRENT SERIES</h3>
+      </div>
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 py-5">
-
         <CardItem :src="comic.thumb" :title="comic.series" v-for="comic in comics" />
-
+      </div>
+      <div class="d-flex justify-content-center pb-4">
+        <button type="button" class="btn btn-primary">Load More</button>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/_variables.scss" as *;
+  .main_top{
+    position: relative;
+  }
+  #title{
+    padding: 0.6rem;
+    background-color: $primary;
+    position: absolute;
+    float: left;
+    top: -26.39px;
+    h3{
+      margin: 0;
+    }
+  }
+
   #banner{
     height: 600px;
     background-image: url(../assets/img/jumbotron.jpg);
