c336619cb1622b6f81ff64497f51b9b67b22a6ca
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-14 12:05:22 +0200

    Fix variables

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,74 +1,75 @@
 <script>
 export default {
-    name: "SiteHeader",
-    data(){
-      return {
-        menu: [
-          {
-            text: 'CHARACTERS',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'COMICS',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'MOVIES',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'TV',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'GAMES',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'COLLECTIBLES',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'VIDEOS',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'FANS',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'NEWS',
-            href: '#',
-            currentPage: false,
-          },
-          {
-            text: 'SHOP',
-            href: '#',
-            currentPage: false,
-          },
-        ]
-
-      }
-    }
-}
+  name: "SiteHeader",
+  data() {
+    return {
+      menu: [
+        {
+          text: "CHARACTERS",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "COMICS",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "MOVIES",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "TV",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "GAMES",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "COLLECTIBLES",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "VIDEOS",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "FANS",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "NEWS",
+          href: "#",
+          currentPage: false,
+        },
+        {
+          text: "SHOP",
+          href: "#",
+          currentPage: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-    <header id="app_header" class="py-2">
+  <header id="app_header" class="py-2">
     <div class="container d-flex align-items-center justify-content-between">
       <div class="logo">
         <img src="../assets/img/dc-logo.png" alt="" />
       </div>
       <div class="nav">
-        <div v-for="item in menu"><a href="#">{{ item.text }}</a></div>
+        <div v-for="item in menu">
+          <a href="#">{{ item.text }}</a>
+        </div>
         <!-- <div><a href="#">COMICS</a></div>
         <div><a href="#">MOVIES</a></div>
         <div><a href="#">TV</a></div>
@@ -83,6 +84,26 @@ export default {
   </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../styles/partials/_variables.scss" as *;
 
+header {
+  background-color: white;
+}
+.nav {
+  gap: 1rem;
+  
+  a {
+    text-decoration: none;
+    color: rgb(92, 92, 92);
+    font-weight: 500;
+    height: 100%;
+
+    &:hover {
+      cursor: pointer;
+      color: $primary;
+      border-bottom: 4px solid $primary;
+    }
+  }
+}
 </style>
