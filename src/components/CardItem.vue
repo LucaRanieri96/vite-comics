17e0c242f51b007cf319c3e0975b094ff750e2b4
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-14 15:37:12 +0200

    Add CardItem.vue

---
+++ src/components/CardItem.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+  name: "CardItem",
+  props: {
+    title: String,
+    src: String
+  }
+};
+</script>
+
+<template>
+  <div class="col">
+    <div>
+        <img :src="thumb" :alt="type">
+        <h3> {{ series }} </h3>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,19 +1,34 @@
 <script>
+import comics from "../data/comics.js"
+
 export default {
   name: "SiteMain",
   data() {
-    return {};
+    return {
+      comics : comics,
+    };
   },
 };
 </script>
 
 <template>
+  <div id="banner"></div>
+
   <div class="main_top">
     <div class="container">
-      <div class="py-5">Content goes here</div>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6">
+
+        <CardItem :src="comic.thumb" :title="comic.series" v-for="comic in comics" />
+
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+  #banner{
+    height: 600px;
+    background-image: url(../assets/img/jumbotron.jpg);
+    background-size: cover;
+  }
 </style>
